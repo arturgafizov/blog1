100acf4349a2e57fc69b7d32efe289fba81cc7ca
Artur <art05xxx@rambler.ru>
2021-03-16 08:09:02 +0300

    14 commit  add in url path change-password

--- web/users/urls.py
+++ web/users/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 
-from users.views import UserRetrieveView, UploadAvatarView
+from users.views import UserRetrieveView, UploadAvatarView, ChangeUserPasswordView
 
 urlpatterns = [
     path('', UserRetrieveView.as_view(), name='profile-detail'),
     path('avatar/', UploadAvatarView.as_view(), name='avatar_upload'),
+    path('change-password/', ChangeUserPasswordView.as_view(), name='change_password'),
     # path('', UserList.as_view(), name='user-list'),
 ]
