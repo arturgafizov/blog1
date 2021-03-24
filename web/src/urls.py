767ecb8214260501e921c897205082f8e0e7bd0d
Artur <art05xxx@rambler.ru>
2021-03-24 07:19:15 +0300

    19 commit  add registrations url

--- web/src/urls.py
+++ web/src/urls.py
@@ -2,17 +2,24 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from django.conf.urls import include, url
 from .yasg import urlpatterns as swagger_url
+from django.views.generic import TemplateView
 
 admin_url = settings.ADMIN_URL
 
 urlpatterns = [
     path('', include('main.urls')),
     path('users/', include('users.urls')),
+    path('registrations/', include('registrations.urls')),
     path(f'{admin_url}/', admin.site.urls),
     path(f'{admin_url}/defender/', include('defender.urls')),
     path('api/', include('rest_framework.urls')),
+    path('registration/', TemplateView.as_view(template_name="signup.html"),
+        name='signup'),
+    path('email-verification/',
+        TemplateView.as_view(template_name="email_verification.html"),
+        name='email-verification'),
 
 ]
 
