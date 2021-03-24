6565d3c06fe4bfff165c5221b09a241a796e5f94
Artur <art05xxx@rambler.ru>
2021-03-24 07:24:50 +0300

    22 commit  add urls in local urls.py

--- web/users/urls.py
+++ web/users/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 
-from users.views import UserRetrieveView, UploadAvatarView, ChangeUserPasswordView
+from users.views import UserRetrieveView, UploadAvatarView
+
 
 urlpatterns = [
     path('', UserRetrieveView.as_view(), name='profile-detail'),
     path('avatar/', UploadAvatarView.as_view(), name='avatar_upload'),
-    path('change-password/', ChangeUserPasswordView.as_view(), name='change_password'),
-    # path('', UserList.as_view(), name='user-list'),
+
+]
+
+urlpatterns += [
+    path('verify/<token>/', TemplateView.as_view(), name='account_confirm_email'),
+    path('verify/', TemplateView.as_view(), name='account_email_verification_sent'),
 ]
