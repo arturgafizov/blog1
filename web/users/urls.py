d22c1c9ebb78fae9dbf5ca63e830153409db9bba
Artur <art05xxx@rambler.ru>
2021-03-26 07:35:01 +0300

    29 commit  update urls, and migrations files in app  users

--- web/users/urls.py
+++ web/users/urls.py
@@ -12,8 +12,3 @@ urlpatterns = [
     path('avatar/', UploadAvatarView.as_view(), name='avatar_upload'),
 
 ]
-
-urlpatterns += [
-    path('verify/<token>/', TemplateView.as_view(), name='account_confirm_email'),
-    path('verify/', TemplateView.as_view(), name='account_email_verification_sent'),
-]
