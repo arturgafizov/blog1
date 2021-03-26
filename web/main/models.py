d5c23b65bd34f2bca6a3623a15842308536de1fd
Artur <art05xxx@rambler.ru>
2021-03-26 07:32:30 +0300

    27 commit  update model User

--- web/main/models.py
+++ web/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
-
+from allauth.account.models import EmailAddress
 
 class User(AbstractUser):
 
@@ -17,6 +17,16 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+    def full_name(self):
+        return self.get_full_name()
+
+    def email_verified(self):
+        email = self.emailaddress_set.filter(primary=True).first()
+        return email.verified if email else False
+
+    email_verified.boolean = True
+
 
     def __str__(self):
         return self.email
