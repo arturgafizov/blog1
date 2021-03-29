508433272c49ae6606910f700c59b6d726407cb9
Artur <art05xxx@rambler.ru>
2021-03-29 07:25:14 +0300

    33 commit  add def country in models.py

--- web/main/models.py
+++ web/main/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from .managers import UserManager
 from allauth.account.models import EmailAddress
 
+
 class User(AbstractUser):
 
     username = None
@@ -21,12 +22,14 @@ class User(AbstractUser):
     def full_name(self):
         return self.get_full_name()
 
+    def country(self):
+        return self.Address.objects.first()
+
     def email_verified(self):
         email = self.emailaddress_set.filter(primary=True).first()
         return email.verified if email else False
 
     email_verified.boolean = True
-
 
     def __str__(self):
         return self.email
