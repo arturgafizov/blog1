3ae7d31a1b7d2bbd130015de2cb87ac210084dc6
Artur <art05xxx@rambler.ru>
2021-03-29 07:24:35 +0300

    32 commit  add search_fields in admin.py

--- web/main/admin.py
+++ web/main/admin.py
@@ -1,7 +1,10 @@
+import csv
+
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
@@ -11,7 +14,9 @@ User = get_user_model()
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     ordering = ()
-    list_display = ('email', 'full_name', 'is_active', 'email_verified')
+    list_display = ('email', 'full_name', 'is_active', 'email_verified', )
+    # list_filter = ('emailaddress_set__verified',)
+    search_fields = ['address_set__city', ]
 
     fieldsets = (
         (None, {'fields': ('password',)}),
