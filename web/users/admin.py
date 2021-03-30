23e19076ad9b6c88c5cf33c2448ad5c9eb3172bb
Artur <art05xxx@rambler.ru>
2021-03-30 07:31:20 +0300

    35 commit  add fields admin.py models Article? Category

--- web/users/admin.py
+++ web/users/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from users.models import Profile
+
+from users.models import Profile, Address
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'mobile', 'location')
+    list_display = ('user', 'mobile', 'location', )
+    search_fields = ['location', ]
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('country', 'user', 'city',)
+    search_fields = ('city',)
