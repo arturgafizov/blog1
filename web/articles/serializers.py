01ee34223055cf4bc941ed344472a3413186e0a4
Artur <art05xxx@rambler.ru>
2021-04-04 15:13:17 +0300

    43 commit  split into ShortArticleSerializer and ArticleSerializer, add CategotySerializer

--- web/articles/serializers.py
+++ web/articles/serializers.py
@@ -1,19 +1,44 @@
 from rest_framework import serializers
 
-from .models import Article, Category
-
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = (
-            'title', 'content', 'category', 'created', 'updated',
-        )
+from .models import Article, Category, Comment
+from main.serializers import UserSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.SlugField(source='get_absolute_url', allow_unicode=True)
+
     class Meta:
         model = Category
         fields = (
-            'name', 'parent',
+            'id', 'name', 'parent', 'url',
         )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+
+        fields = (
+            'id', 'author', 'content', 'parent', 'updated',
+        )
+
+
+class ShortArticleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    url = serializers.SlugField(source='get_absolute_url', allow_unicode=True)
+    comment_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Article
+        fields = (
+            'id', 'title', 'category', 'created', 'updated', 'image', 'author', 'url', 'comment_count',
+        )
+
+
+class ArticleSerializer(ShortArticleSerializer):
+    comments = CommentSerializer(many=True, source='comment_set')
+
+    class Meta(ShortArticleSerializer.Meta):
+        fields = ShortArticleSerializer.Meta.fields + ('comments', 'content', )
