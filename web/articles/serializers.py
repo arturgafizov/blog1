bb1c967d519b51b3e0e5e2e5f3434c39a89dbdea
Artur <art05xxx@rambler.ru>
2021-04-08 07:36:12 +0300

    49 commit  add def validate and create in CommentSerializer

--- web/articles/serializers.py
+++ web/articles/serializers.py
@@ -15,13 +15,36 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.EmailField(required=False)
 
     class Meta:
         model = Comment
 
         fields = (
-            'id', 'author', 'content', 'parent', 'updated',
+            'id', 'author', 'content', 'parent', 'updated', 'article',
         )
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        user = request.user
+        if not user.is_authenticated and not attrs.get('author'):
+            raise serializers.ValidationError('Введите емаил')
+        return attrs
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+        if user.is_authenticated:
+            validated_data['author'] = user.email
+            validated_data['user'] = user
+        return Comment.objects.create(**validated_data)
+        # return Comment.objects.create(
+        #     author=validated_data['author'],
+        #     content=validated_data['content'],
+        #     article=validated_data['article'],
+        #     parent=validated_data.get('parent'),
+        #     user=validated_data.get('user'),
+        # )
 
 
 class ShortArticleSerializer(serializers.ModelSerializer):
@@ -42,3 +65,6 @@ class ArticleSerializer(ShortArticleSerializer):
 
     class Meta(ShortArticleSerializer.Meta):
         fields = ShortArticleSerializer.Meta.fields + ('comments', 'content', )
+
+
+
